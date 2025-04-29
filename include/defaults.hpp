c6042a872138a84854699281efdcd50ad25305fc
Andrew Pegg <apegg@ucsd.edu>
2025-04-28 23:09:24 -0700

    small changes to defualt values, and clean up command line argument setup

--- include/defaults.hpp
+++ include/defaults.hpp
@@ -8,6 +8,7 @@ const int DEFAULT_HEIGHT = 960;
 // assume standard 30 frames a second for stream
 const int DEFAULT_FRAMES = 30;
 
-// assume stream default encoding to NV12
-const std::string DEFAULT_ENCODING = "NV12";
-const std::string DEFAULT_AI_ENCODING = "RGB";
+// assume stream default encoding to NV12 and assume AI is feed raw RBG frames no encoding
+const std::string DEFAULT_RAW_FORMAT = "NV12";
+const std::string DEFAULT_AI_RAW_FORMAT = "RGB";
+const std::string DEFAULT_ENCODING = "v4l2h264enc";
--- src/main.cc
+++ src/main.cc
@@ -9,10 +9,14 @@ int main() {
     printf("hello world!\n");
     argparse::ArgumentParser program("gst-open-gate-rtsp-sender",version);
     program.add_description("utility for sending video and audio data over udp to an rtsp endpoint\n");
-    program.add_argument("-w","-width").help("width of capture, default: 1280").scan<'d',int>();
-    program.add_argument("-h","-height").help("height of capture, default: 960").scan<'d',int>();
-    program.add_argument("-enc","-encode").help("encoding of stream, default: NV12");
-    program.add_argument("-ai_enc").help("AI inference stream encoding, default: rgb");
-    program.add_argument("-port","-p").help("two ports one being the video track port and the other being the audio track").nargs(2).scan<'d',int>();
+    program.add_argument("ip").help("ip address of the rtsp server, required and not provided\n");
+    program.add_argument("-w","-width").help("width of capture, default: "+std::to_string(DEFAULT_WIDTH)).default_value(std::to_string(DEFAULT_WIDTH)).scan<'d',int>();
+    program.add_argument("-h","-height").help("height of capture, default: "+std::to_string(DEFAULT_HEIGHT)).default_value(std::to_string(DEFAULT_HEIGHT)).scan<'d',int>();
+    program.add_argument("-raw","-raw_format").help("encoding of stream, default: NV12").default_value(DEFAULT_RAW_FORMAT);
+    program.add_argument("-ai_raw").help("AI inference stream format, default: RGB").default_value(DEFAULT_AI_RAW_FORMAT);
+    program.add_argument("-ai_udp").help("specify if you want gstreamer to dump video data over udp instead of appsink").default_value(false).implicit_value(true);
+    program.add_argument("-ai_port").help("-ai_port [port] if -ai_udp is present a port needs to be provided");
+    program.add_argument("-port","-p").help("[port1] [port2] port 1 is video track, port 2 is audio track").nargs(2).default_value(std::vector<std::string>{"5000","5001"}).scan<'d',int>();
+    program.add_epilog("The program ships with defaults for most of these paramater and their providing is only need if configuration of the system is non-default");
     return 0;
 }
